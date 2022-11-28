c7a608da9baa024006bfca5300a13e5368b7947d
User0332 <carlzfurtado@gmail.com>
2022-11-28 13:30:47 -0500

    add speed test folder

---
+++ tests/speed/c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* other()
+{
+    return "ok";
+}
+
+int main()
+{
+    printf("%s%s%s%s%s%s%s", other(), other(), other(), other(), other(), other(), other());
+
+    return 0+0+0+0+0+0-0-0-0-0-0-0;
+}
